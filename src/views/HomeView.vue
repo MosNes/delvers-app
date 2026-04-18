dc3102bc49fdd0864b07fb990ad2799acc997f51
MosNes <7hekarl@gmail.com>
2026-04-18 15:50:49 -0400

    removed starter coe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+      This Page is under Construction
   </main>
 </template>
