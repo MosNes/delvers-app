5b7212391925fc7da0781e4063775a1a6906807a
MosNes <7hekarl@gmail.com>
2026-04-28 20:08:57 -0400

    installed and configured primevue to use volt, updated base.css variables

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import './assets/main.css'
+import PrimeVue from 'primevue/config';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+
+// tell Vue to use PrimeVue and enable unstyled mode to work with Volt and TailwindCSS
+app.use(PrimeVue, {
+    unstyled: true,
+});
 
 app.use(router)
 
