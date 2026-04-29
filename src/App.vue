c6681f74b464b2ece091e81ab3c7f424fe75c3f6
MosNes <7hekarl@gmail.com>
2026-04-28 20:16:38 -0400

    installed volt panel component

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
 
-  <RouterView />
+  <div>
+
+  </div>
+  <!-- <RouterView /> -->
 </template>
