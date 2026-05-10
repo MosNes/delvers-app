5db24d478ac384b3c78d3cfd060a2fd503ed3163
MosNes <7hekarl@gmail.com>
2026-05-10 09:23:00 -0400

    added reactive data to character sheet, hardcoded test character data

--- src/views/CharacterSheet.vue
+++ src/views/CharacterSheet.vue
@@ -1,7 +1,58 @@
+<!-- the character sheet view displays all the information for a character, including their attributes, skills, talents, and inventory. -->
 <script setup>
+//import vue features
+import {reactive} from 'vue';
+
+//import components
 import Panel from '@/volt/Panel.vue';
 import Divider from '@/volt/Divider.vue';
 import AttributeCard from '@/components/AttributeCard.vue';
+
+//test character data
+// actual character data will be imported from pinia store
+const charData = reactive({
+    owner: "test@test.com",
+    campaign: 1,
+    imgUrl: "https://cdn.pixabay.com/photo/2024/04/21/09/20/ai-generated-8710168_640.jpg",
+    characterName: "Jimothy P Frogman",
+    player: "Test Player",
+    ancestry: "Xolotl",
+    ancestrySpecies: "Green Tree Frog",
+    destiny: 1,
+    path: "Path of the Warrior",
+    background: "Test Background",
+    domains: ["Froggery", "Mannerisms"],
+    skills: ["Frogmannerisms"],
+    talents: ["Favored Weapon"],
+    advances: [],
+    minorAdvances: 0,
+    majorAdvances: 0,
+    pinnacleAdvances: 0,
+    maxGuard: 4,
+    currentGuard: 4,
+    armor: 0,
+    maxBody: 10,
+    currentBody: 10,
+    maxSpeed: 11,
+    currentSpeed: 11,
+    maxMind: 8,
+    currentMind: 8,
+    maxSpirit: 13,
+    currentSpirit: 13,
+    blessings: 0,
+    curses: 0,
+    doom: 0,
+    bodyStress: false,
+    speedStress: false,
+    mindStress: false,
+    spiritStress: false,
+    notes: "JIMMY FROGMAN IS A FROGMAN"
+});
+
+// sheet view state data
+const viewData = reactive({
+});
+
 </script>
 
 <template>
