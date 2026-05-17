397e8d6902122dc9ad3d8b8762e4611661ad9954
MosNes <7hekarl@gmail.com>
2026-05-17 17:20:36 -0400

    tested login with magic link, user and user_profile created successfully, no Name field in user_profile though.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import CharacterSheet from './views/CharacterSheet.vue';
+import Auth from './components/Auth.vue';
 </script>
 
 <template>
